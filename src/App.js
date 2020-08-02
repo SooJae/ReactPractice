494183e84240bb8cac607daa15fb59fadd7f18ac
Jerry <jerry@exntu.com>
2020-08-02 23:42:19 +0900

    [master] 추가 - input 추가, 삭제

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './App.css';
-import InputSample from "./InputSample";
+import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const {username, email} = inputs;
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState(
+    [
+      {
+        id: 1,
+        username: 'soojae',
+        email: 'aa.naver.com',
+        active: true,
+      },
+      {
+        id: 2,
+        username: 'leesu',
+        email: 'bb.naver.com',
+        active: false,
+      },
+      {
+        id: 3,
+        username: 'jaeli',
+        email: 'cc.naver.com',
+        active: false,
+      },
+    ]);
+
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    }
+
+    setUsers([...users, user]);
+    setInputs({
+      username: '',
+      email: ''
+    });
+
+    console.log(nextId.current); // 4
+    nextId.current += 1;
+  }
+
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  }
+
+  const onToggle = id => {
+    setUsers(users.map(
+      user => user.id === id
+        ? {...user, active: !user.active}
+        : user
+    ))
+  }
+
   return (
-    <InputSample/>
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+    </>
   );
 }
 
