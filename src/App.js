b0c85af8734b1c80f4a1e134e42085a56c04375c
soojae <developersoojae@gmail.com>
2020-08-03 21:59:29 +0900

    [master] 추가 - useCallback, React.memo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef, useState} from 'react';
+import React, {useCallback, useMemo, useRef, useState} from 'react';
 import './App.css';
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
@@ -14,13 +14,13 @@ function App() {
     email: '',
   });
   const {username, email} = inputs;
-  const onChange = e => {
+  const onChange = useCallback(e => {
     const {name, value} = e.target;
     setInputs({
       ...inputs,
       [name]: value
     });
-  };
+  },[inputs]);
 
   const [users, setUsers] = useState(
     [
@@ -47,14 +47,14 @@ function App() {
 
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
       email
     }
 
-    setUsers([...users, user]);
+    setUsers(users => [...users, user]);
     setInputs({
       username: '',
       email: ''
@@ -62,19 +62,19 @@ function App() {
 
     console.log(nextId.current); // 4
     nextId.current += 1;
-  }
+  }, [username, email]);
 
-  const onRemove = id => {
-    setUsers(users.filter(user => user.id !== id));
-  }
+  const onRemove = useCallback(id => {
+    setUsers(users => users.filter(user => user.id !== id));
+  }, []);
 
-  const onToggle = id => {
-    setUsers(users.map(
+  const onToggle = useCallback(id => {
+    setUsers(users => users.map(
       user => user.id === id
         ? {...user, active: !user.active}
         : user
     ))
-  }
+  }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
   return (
