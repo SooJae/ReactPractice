512fb17c1499fc293683938a2b857ea2b055c6a6
soojae <developersoojae@gmail.com>
2020-08-06 22:10:35 +0900

    [master] 리액트

--- useful-tools/src/App.js
+++ useful-tools/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const a = '123';
 function App() {
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
   return (
     <div className="App">
       <header className="App-header">
