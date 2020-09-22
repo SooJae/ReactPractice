927c19040e709c5c4d29bed7061dd717b52aad85
soojae <developersoojae@gmail.com>
2020-09-22 21:48:50 +0900

    [master] Redux 리덕스

--- learn-redux/src/containers/CounterContainer.js
+++ learn-redux/src/containers/CounterContainer.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Counter from '../components/Counter';
-import { useDispatch, useSelector } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { decrease, increase, setDiff } from '../modules/counter';
 
 function CounterContainer(props) {
   // store.getState() 와 같은 기능
-  const { number, diff } = useSelector(state => ({
-    number: state.counter.number,
-    diff: state.counter.diff
-  }));
+  // const { number, diff } = useSelector(state => ({
+  //   number: state.counter.number,
+  //   diff: state.counter.diff
+  // }));
 
+  // 첫번째 방법
+  //const number = useSelector(state => state.counter.number);
+  //const diff = useSelector(state => state.counter.diff);
+
+  // 두번째 방법
+  const { number, diff } = useSelector(
+    state => ({
+      number: state.counter.number,
+      diff: state.counter.diff
+    }),
+    shallowEqual
+    //(left, right) => {
+    //  return left.diff === right.diff && left.number === right.number;
+    // }
+  );
   const dispatch = useDispatch();
 
   const onIncrease = () => dispatch(increase());
