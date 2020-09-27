7b97e2c173a1d2b7325b1e8a711f873650113ae8
soojae <developersoojae@gmail.com>
2020-09-27 22:17:36 +0900

    [master] Redux 미들웨어 - Route 뒤로가기시 로딩중 상태 안 나오도록

--- learn-redux-middleware/src/containers/PostContainer.js
+++ learn-redux-middleware/src/containers/PostContainer.js
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Post from '../components/Post';
-import { getPost } from '../modules/posts';
+import { clearPost, getPost } from '../modules/posts';
 
 const PostContainer = ({ postId }) => {
   const { data, loading, error } = useSelector(state => state.posts.post);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getPost(postId));
+    return () => {
+      dispatch(clearPost());
+    };
   }, [postId, dispatch]);
 
   if (loading) return <div>로딩중...</div>;
